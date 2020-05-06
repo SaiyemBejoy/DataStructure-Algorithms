787088d2ca666d92e7cbcd9d74ef2fd4efc3e6ae
SaiyemBejoy <jsbejoy96@gmail.com>
2020-05-07 02:18:54 +0600

    Mosh part1 Arrays

---
+++ Data_Structure_&_Algorithms_Mosh/src/data_structure/ArrayMain.java
@@ -0,0 +1,17 @@
+package data_structure;
+
+public class ArrayMain {
+    public static void main(String[] args) {
+        Array numbers = new Array(3);
+        numbers.insert(10);
+        numbers.insert(20);
+        numbers.insert(30);
+        numbers.insert(40);
+
+        numbers.removeAt(3);
+
+        numbers.print();
+
+        System.out.println("\t" + numbers.indexOf(20));
+    }
+}
